e5a8a0c05283c16ca0cbeed618cbb5a06e90e01f
vyaches <sboriskin@gmail.com>
2020-04-15 13:26:04 +0300

    Написать линейную программу: • Определить 2 или 3 переменные целого (int) или вещественного типа (double) • Посчитать арифметическое выражение, используя эти переменные • Вывести результат в консоль

---
+++ src/lesson1/Main.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+public class Main {
+    public static void main(String args[]) {
+        int a = 6;
+        int b = 2;
+        int c = 5;
+
+        double result = 0;
+
+        if (a == 0)
+            System.out.println("Деление на ноль недопустимо");
+        else
+            result = (double) (b + c) / a;
+        System.out.println("Полученный результат: " + result);
+
+    }
+}
